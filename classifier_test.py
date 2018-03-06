cc46c9a3b16c7770d0ec7741b99475f8d7eeb274
mitchdull <mdull@stevens.edu>
2018-03-06 18:41:07 -0500

    commented file for clarity

--- classifier_test.py
+++ classifier_test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 '''
-Classifier
+Classification Test
 '''
 
 # load dataset from pickled numpy files
@@ -15,22 +15,18 @@ dataset = np.load("dataset.npy")
 # normalize dataset
 dataset = preprocessing.scale(dataset)
 
-# initialize svc
-# first try
-# C = 1.0  # SVM regularization parameter
-# train svc
-# svc = svm.SVC(kernel='linear', C=C).fit(training_scaled, classes)
-# with multiclass
-
+# split train/test data 50/50
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(dataset, class_labels, test_size=0.5)
 
+#train classifier
 clf = OneVsRestClassifier(svm.LinearSVC(random_state=0)).fit(X_train, y_train)
-# give
+
+# generate predictions for test data
 predict = clf.predict(X_test)
 
-result_sum = (predict == y_test).sum()
-accuracy = result_sum/2089
-print(result_sum)
-print(accuracy*100)
+# percent correctly predicted
+result_sum = float((predict == y_test).sum())
+print((result_sum/len(X_test)))
 
+# Accuracy on train/test split
 print(clf.score(X_test, y_test))
